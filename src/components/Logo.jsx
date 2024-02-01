945e2c26eb91427119ee90974dc871be9d1f34e9
Kevin Loughead <kvnloughead@gmail.com>
2024-02-01 14:55:16 -0500

    fix: fix images

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,11 @@
 import "./styles/Logo.css";
+import logo from "../assets/images/logo.png";
 
 function Logo(props) {
   return (
     <div className="logo">
       <div className="logo__container">
-        <img
-          className="logo__image"
-          src="/images/logo.png"
-          alt="CryptoDucks logo"
-        />
+        <img className="logo__image" src={logo} alt="CryptoDucks logo" />
         {props.title && <p className="logo__title">{props.title}</p>}
       </div>
     </div>
